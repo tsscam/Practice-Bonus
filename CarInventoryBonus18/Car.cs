e66eb38c6344876d6cfc5e4075be947b9d9f360a
Theresa Skonieczny <tsscamaro@gmail.com>
2017-06-07 16:54:16 -0400

    first commit

---
+++ CarInventoryBonus18/Car.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInventoryBonus18
+{
+    class Car
+    {   
+        //identifiers of the Car object
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        //Properties of the Car class with Getters & Setters.....
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+
+            set
+            {
+                make = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+
+            set
+            {
+                model = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+
+            set
+            {
+                year = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        //this constructor initiates a value for each property
+        public Car()
+        {
+            make = "";
+            model = "";
+            year = 0;
+            price = 0.0;
+
+
+        }
+
+        //this constructor enables the user to input the value of the property
+        public Car(string CarMake, string CarModel, int CarYear, double CarPrice)
+        {
+            make = CarMake;
+            model = CarModel;
+            year = CarYear;
+            price = CarPrice;
+        }
+    }
+}
---
+++ CarInventoryBonus18/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInventoryBonus18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            
+
+            //Message displayed to user
+            Console.WriteLine("Enter the Make of the Vehicle : ");
+            //Converts the user input into a string
+            string CarMake = Console.ReadLine();
+
+            Console.WriteLine("Enter the Model of the Vehicle : ");
+            string CarModel = Console.ReadLine();
+
+            Console.WriteLine("Enter the Year of the Vehicle : ");
+            string CarYear = Console.ReadLine();
+
+            Console.WriteLine("Enter the Price of the Vehicle : ");
+            string CarPrice = (Console.ReadLine());
+
+
+
+            //Converts the String by Parsing the data and returning a double and storing in the object.
+            Car1.Make = CarMake;
+            Car1.Model = CarModel;
+            Car1.Year = int.Parse(CarYear);
+            Car1.Price = Double.Parse(CarPrice);
+
+            //Moved from here to the bottom, to display the inputed info and the Current List
+            //Console.WriteLine("The Car You Chose is {0}, {1}, {2}, {3}.", Car1.Make, Car1.Model, Car1.Year, Car1.Price);
+
+            //Created List for CARS added in and what is to be added
+            List<Car> CarList = new List<Car>();
+            
+            //Code used to ADD new CARS to the LIST
+            CarList.Add(Car1);
+
+            //Adding a CAR "Ford, Escort, 2012, 65000" to the list
+            CarList.Add(new Car("Ford", "Escort", 2012, 65000));
+
+            //for (int i = 0; i < CarList.Count; i++)
+            //{
+            //    Console.WriteLine(CarList[i]);
+            //}
+            //Used a foreach to display the NewCars in the Carlist
+            foreach (var NewCar in CarList)
+            {
+                Console.WriteLine("The Inventory shows {0}, {1}, {2}, {3}.", NewCar.Make, NewCar.Model, NewCar.Year, NewCar.Price);
+
+            }
+        }
+    }
+}
